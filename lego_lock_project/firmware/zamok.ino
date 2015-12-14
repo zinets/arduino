4abbba1d04a52d445f2216a40c975eda4c6e72c9
Zinets Victor <victor.zinets@together.com>
2015-12-14 12:14:30 +0200

    ++

--- lego_lock_project/firmware/zamok.ino
+++ lego_lock_project/firmware/zamok.ino
@@ -1,5 +1,16 @@
-void setup() {
+// hardware abstraction layer
+#define 	COL1	12
+#define 	COL2	11
+#define 	COL3	10
 
+#define 	ROW1	9
+
+void setup() {
+	pinMode(COL1, INPUT);
+	pinMode(COL2, INPUT);
+	pinMode(COL3, INPUT);
+
+	pinMode(ROW1, OUTPUT);
 }
 
 void loop() {
