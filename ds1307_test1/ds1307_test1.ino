2d420b3ba2652b48659e21c0100e746f7554fd6f
Viktor Zinets <Viktor Zinets>
2016-04-23 14:24:29 +0300

    ++ test sketch with dht11 & ds1307

---
+++ ds1307_test1/ds1307_test1.ino
@@ -0,0 +1,119 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+RTC_DS1307 RTC;
+const byte interruptPin = 2;
+const byte ledPin = 13;
+volatile byte state = LOW;
+
+
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+void showTime () {
+  state = !state;
+
+  DateTime now;// = RTC.now();
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(' ');
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  // Serial.println();
+  // Serial.print(" since 1970 = ");
+  // Serial.print(now.unixtime());
+  // Serial.print("s = ");
+  // Serial.print(now.unixtime() / 86400L);
+  // Serial.println("d");
+  // // рассчитываем дату: 7 дней и 30 секунд
+  // DateTime future (now.unixtime() + 7 * 86400L + 30);
+  // Serial.print(" now + 7d + 30s: ");
+  // Serial.print(future.year(), DEC);
+  // Serial.print('/');
+  // Serial.print(future.month(), DEC);
+  // Serial.print('/');
+  // Serial.print(future.day(), DEC);
+  // Serial.print(' ');
+  // Serial.print(future.hour(), DEC);
+  // Serial.print(':');
+  // Serial.print(future.minute(), DEC);
+  // Serial.print(':');
+  // Serial.print(future.second(), DEC);
+  // Serial.println();
+}
+
+void setup () {
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(57600);
+  Wire.begin();
+  RTC.begin();
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+
+
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Temperature");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");
+  Serial.println("------------------------------------");
+
+  dht.humidity().getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.println("Humidity");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");
+  Serial.println("------------------------------------");
+
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+  else {
+    Serial.print("Temperature: ");
+    Serial.print(event.temperature);
+    Serial.println(" *C");
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println("Error reading humidity!");
+  }
+  else {
+    Serial.print("Humidity: ");
+    Serial.print(event.relative_humidity);
+    Serial.println("%");
+  }
+}
+
+void loop() {
+
+}
--- weather_show/weather_show.ino
+++ weather_show/weather_show.ino
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 #include "U8glib.h"
 
 #include "resources.h"
