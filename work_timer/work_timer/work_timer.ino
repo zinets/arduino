e7d59de900d1f95801897d78063985b88cd32446
zinets <zinetz.victor@gmail.com>
2017-02-04 17:15:05 +0200

    ++  подключение rtc, вывод времени на lcd

--- work_timer/work_timer/work_timer.ino
+++ work_timer/work_timer/work_timer.ino
@@ -1,14 +1,54 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <Wire.h>
+#include "RTClib.h"
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 9, 8, 7, 6);
+RTC_DS1307 rtc;
+
+enum MainState {
+    stateTimeUpdated,
+    stateInactive,
+};
+volatile MainState mainState = stateInactive;
 
 void setup() {
+  Serial.begin(57600);
+  Wire.begin();
+  rtc.begin();
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    rtc.adjust(DateTime(__DATE__, __TIME__));
+  }
+  rtc.writeSqwPinMode(SquareWave1HZ);
+
   lcd.begin(16, 2);
-  lcd.print("Hello, world! Абв");
+  attachInterrupt(0, ping, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switch (mainState) {
+    case stateTimeUpdated: {
+      DateTime now = rtc.now();
+      String time;
+      int sec = now.second();
+      if (sec % 2 == 0) {
+        time = String(now.hour()) + ":" + String(now.minute());
+      } else {
+        time = String(now.hour()) + " " + String(now.minute());
+      }
+      lcd.setCursor(0, 1);
+      lcd.print(time);
 
+      mainState = stateInactive;
+    } break;
+    case stateInactive:
+    default:
+
+      break;
+  }
+}
+
+void ping() {
+  mainState = stateTimeUpdated;
 }
