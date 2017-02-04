bfb047cf92cb84e1c25a4bd847ad73d377a32f2d
zinets <zinetz.victor@gmail.com>
2017-02-04 13:07:10 +0200

    ++ initial commit

---
+++ work_timer/work_timer/work_timer.ino
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Hello, world! Абв");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
