1d70bcbd3a25d4345ddb0c945e33a6974a89c13a
Zinets Victor <victor.zinets@together.com>
2017-02-22 14:31:48 +0200

    ++ storing last time in eeprom

--- work_timer/work_timer/work_timer.ino
+++ work_timer/work_timer/work_timer.ino
@@ -2,6 +2,7 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
 #include <Bounce2.h>
+#include <EEPROM.h>
 
 #include "RTClib.h"
 
@@ -9,7 +10,8 @@ LiquidCrystal lcd(12, 11, 9, 8, 7, 6);
 RTC_DS1307 rtc;
 char buf[16];
 Bounce bouncer = Bounce();
-#define BUTTON_PIN 3
+#define BUTTON_PIN    3
+#define EEPROM_ADDR   0
 
 byte divider[8] = {
   B01100,
@@ -49,9 +51,14 @@ enum MainState {
     stateInactive,
 };
 volatile MainState mainState = stateInactive;
-bool timerStarted;
 
-DateTime arriveTime, endTime;
+struct TimeObject {
+  bool timerStarted;
+  DateTime arriveTime;
+  DateTime endTime;
+}
+
+struct TimeObject timeStruct;
 
 void setup() {
   Serial.begin(57600);
@@ -73,8 +80,8 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT);
   bouncer.attach(BUTTON_PIN);
   bouncer.interval(5);
-
-  timerStarted = false;
+  
+  EEPROM.get(EEPROM_ADDR, timeStruct);  
 }
 
 void loop() {
@@ -93,23 +100,25 @@ void loop() {
       }
       lcd.setCursor(0, 1);
       lcd.print(buf);
-      if (timerStarted) {
-        TimeSpan remain = endTime - now;
+      if (timerStruct.timerStarted) {
+        TimeSpan remain = timeStruct.endTime - now;
         if (remain.totalseconds() > 1) {
           // print arrive time
           lcd.setCursor(9, 0);
-          sprintf(buf, "%c %02d:%02d", byte(1), arriveTime.hour(), arriveTime.minute());
+          sprintf(buf, "%c %02d:%02d", byte(1), timeStruct.arriveTime.hour(), timeStruct.arriveTime.minute());
           lcd.print(buf);
           // print remain time
           lcd.setCursor(9, 1);
           sprintf(buf, "%c %02d:%02d", byte(2), remain.hours(), remain.minutes());
           lcd.print(buf);
         } else {
-          timerStarted = false;
+          timeStruct.timerStarted = false;
+          
           lcd.setCursor(9, 0);
           lcd.print("       ");
           lcd.setCursor(9, 1);
-          lcd.print("       ");
+          lcd.print("       ");         
+          EEPROM.put(EEPROM_ADDR, timeStruct);
         }
       }
       mainState = stateInactive;
@@ -118,9 +127,10 @@ void loop() {
     default:
       if (!timerStarted && bouncer.update()) {
         if (bouncer.fell()) {
-          timerStarted = true;
-          arriveTime = rtc.now();
-          endTime = arriveTime + TimeSpan(9 * 60 * 60 + 59);
+          timeStruct.timerStarted = true;
+          timeStruct.arriveTime = rtc.now();
+          timeStruct.endTime = timeStruct.arriveTime + TimeSpan(9 * 60 * 60 + 59);
+          EEPROM.put(EEPROM_ADDR, timeStruct);
         }
       }
       break;
