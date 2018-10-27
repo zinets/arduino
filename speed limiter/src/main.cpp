ba0607e07d0001cc31e8863d0791a0c6c6c903db
zinets <zinetz.victor@gmail.com>
2018-10-27 21:07:22 +0300

    ++

--- speed limiter/src/main.cpp
+++ speed limiter/src/main.cpp
@@ -8,40 +8,39 @@
 #include <Bounce2.h>     // id: 1106
 
 #ifdef TARGET_ESP8266
+#warning BUILDING for esp
 static const int RXPin = 12, TXPin = 13;
 static const uint32_t GPSBaud = 9600;
-static const int audioAlarmPin = 14; // D5
-static const int buttonPin = 16;
-#warning BUILDING for esp
+static const int audioAlarmPin = 14;  // D5
+static const int buttonPin = 16;      // D0
 #endif
 
 #ifdef TARGET_AVR
+#warning BUILDING for avr
 static const int RXPin = 12, TXPin = 13;
 static const uint32_t GPSBaud = 9600;
 static const int audioAlarmPin = 12;
-static const int buttonPin = 16;
-#warning BUILDING for avr
+static const int buttonPin = 11;
 #endif
 
 GpsSensor *gpsSensor;
 OledDisplay *oledDisplay;
 AudioAlarm alarm(audioAlarmPin);
-// Bounce debouncer = Bounce(); 
+Bounce debouncer = Bounce(); 
 
 void setup() {
   Serial.begin(9600);
+
   #ifdef DEBUG
   Log.begin(LOG_LEVEL_VERBOSE, &Serial);
-
-  Log.notice("setup() started");
   #endif
 
   // gpsSensor = new GpsSensor(RXPin, TXPin, GPSBaud);
 
   oledDisplay = new OledDisplay(); 
 
-  // debouncer.attach(buttonPin, INPUT_PULLUP);
-  // debouncer.interval(50);  
+  debouncer.attach(buttonPin, INPUT_PULLUP);
+  debouncer.interval(50);  
 
   alarm.makeStartNoise();
 }   
@@ -55,13 +54,13 @@ void loop() {
        
     // }
     
-    // debouncer.update();    
-    // if (debouncer.fell()) {
-    //   Log.notice("pressed!");
-    //   oledDisplay->changeDrivingMode();
+    debouncer.update();    
+    if (debouncer.fell()) {
+      Log.notice("pressed!");
+      oledDisplay->changeDrivingMode();
 
-    //   alarm.makeTripleBeepNoise();
-    // }
+      alarm.makeTripleBeepNoise();
+    }
   
     alarm.update(ms);
     oledDisplay->update(ms);
