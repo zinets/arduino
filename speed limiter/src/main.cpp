9a0cca7450e56e75ef4e42f00969c342579efe46
zinets <zinetz.victor@gmail.com>
2018-10-27 19:56:31 +0300

    ++ multi boards config

--- speed limiter/src/main.cpp
+++ speed limiter/src/main.cpp
@@ -2,13 +2,24 @@
 #include "gps/gpsSensor.h"
 #include "audioAlarm.h"
 
-#include <ArduinoLog.h>
-#include <Bounce2.h>
+#include <ArduinoLog.h>  // id: 1532
+#include <Bounce2.h>     // id: 1106
 
+#ifdef TARGET_ESP8266
 static const int RXPin = 12, TXPin = 13;
 static const uint32_t GPSBaud = 9600;
-static const int audioAlarmPin = 14;
+static const int audioAlarmPin = 14; // D5
 static const int buttonPin = 16;
+#warning BUILDING for esp
+#endif
+
+#ifdef TARGET_AVR
+static const int RXPin = 12, TXPin = 13;
+static const uint32_t GPSBaud = 9600;
+static const int audioAlarmPin = 12;
+static const int buttonPin = 16;
+#warning BUILDING for avr
+#endif
 
 GpsSensor *gpsSensor;
 OledDisplay *oledDisplay;
@@ -19,11 +30,11 @@ void setup() {
   Serial.begin(9600);
   Log.begin(LOG_LEVEL_VERBOSE, &Serial);
 
-  gpsSensor = new GpsSensor(RXPin, TXPin, GPSBaud);
+  // gpsSensor = new GpsSensor(RXPin, TXPin, GPSBaud);
   oledDisplay = new OledDisplay(); 
 
-  debouncer.attach(buttonPin, INPUT_PULLUP);
-  debouncer.interval(50);  
+  // debouncer.attach(buttonPin, INPUT_PULLUP);
+  // debouncer.interval(50);  
 
   alarm.makeStartNoise();
 }   
@@ -31,17 +42,17 @@ void setup() {
 void loop() {
     unsigned long ms = millis();
 
-    if (gpsSensor->updateGpsData()) {
-      oledDisplay->gpsData = gpsSensor->currentGpsData;
-    }
+    // if (gpsSensor->updateGpsData()) {
+    //   oledDisplay->gpsData = gpsSensor->currentGpsData;
+    // }
     
-    debouncer.update();    
-    if (debouncer.fell()) {
-      Log.notice("pressed!");
-      oledDisplay->changeDrivingMode();
+    // debouncer.update();    
+    // if (debouncer.fell()) {
+    //   Log.notice("pressed!");
+    //   oledDisplay->changeDrivingMode();
 
-      alarm.makeTripleBeepNoise();
-    }
+    //   alarm.makeTripleBeepNoise();
+    // }
   
     alarm.update(ms);
     oledDisplay->update(ms);
