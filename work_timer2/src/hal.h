4c91bb9e17daebe297a4f381b89595d8009692d8
Victor Zinets <victor.zinets@together.com>
2018-11-14 13:21:57 +0200

    ++ new fork for "work_timer"

---
+++ work_timer2/src/hal.h
@@ -0,0 +1,26 @@
+#ifndef HAL_H
+#define HAL_H
+#include <Arduino.h>
+
+#ifdef TARGET_ESP8266
+
+    // button
+    #define BUTTON_PIN    D2
+    // leds
+    #define NUM_LEDS 1
+    #define DATA_PIN 4
+    // eeprom addr
+    #define EEPROM_ADDR   0
+    // one wire sensor
+    #define ONE_WIRE_BUS 5
+    // lcd pins
+    //Define 74HC595 Connections
+    const int Clock = D5;
+    const int Data = D6;
+    const int Latch = D7;
+
+    #define LCD_PINS D5, D6, D7
+#endif
+
+
+#endif
