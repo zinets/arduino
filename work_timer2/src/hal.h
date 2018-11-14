4566756d557e4c16f4bfea2e80538bb81375deaf
zinets <zinetz.victor@gmail.com>
2018-11-14 23:12:07 +0200

    ++

--- work_timer2/src/hal.h
+++ work_timer2/src/hal.h
@@ -5,26 +5,26 @@
 #ifdef TARGET_ESP8266
 
     // button
-    #define BUTTON_PIN    D8
+    #define BUTTON_PIN      D8
     // leds
-    #define LED_TYPE      WS2812
-    #define COLOR_ORDER   GRB
-    #define NUM_LEDS      1
-    #define DATA_PIN      D4
+    #define LED_TYPE        WS2812
+    #define COLOR_ORDER     GRB
+    #define NUM_LEDS        1
+    #define DATA_PIN        D4
     // eeprom addr
-    #define EEPROM_ADDR   0
+    #define EEPROM_ADDR     0
     // one wire sensor
-    #define ONE_WIRE_BUS  5
+    #define ONE_WIRE_BUS    5
     // lcd pins
-    //Define 74HC595 Connections
-    const int Clock = D5;
-    const int Data = D6;
-    const int Latch = D7;
+    // Define 74HC595 Connections
+    // const int Clock = D5;
+    // const int Data = D6;
+    // const int Latch = D7;
 
-    #define LCD_PINS D5, D6, D7
+    #define LCD_PINS        D5, D6, D7
     // rtc
-    #define RTC_SDA     D2
-    #define RTC_SCL     D1
+    #define RTC_SDA         D2
+    #define RTC_SCL         D1
     #define RTC_SQUARE_PIN  D3
 #endif
 
