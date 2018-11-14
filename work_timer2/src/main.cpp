46ffaba2b815a5b7b769d8ac53bac76d570304c8
Victor Zinets <victor.zinets@together.com>
2018-11-14 13:33:45 +0200

    ++

--- work_timer2/src/main.cpp
+++ work_timer2/src/main.cpp
@@ -1,7 +1,33 @@
 #include "hal.h"
+#include "types.h"
+
+#include <LiquidCrystal.h>
+
+// LCD SCREEN
+LiquidCrystal lcd(LCD_PINS);
+volatile LedState ledState = ledStateReady;
+char buf[16];
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  // lcd initialization
+  lcd.begin(16, 2);
+  lcd.createChar(0, divider);
+  lcd.createChar(1, arrived);
+  lcd.createChar(2, exit_symbol);
+  lcd.createChar(3, clock_symbol);
+  lcd.createChar(4, dinner_symbol);
+  lcd.createChar(5, temp_symbol);
+
+  #define DIVIDER_SYMBOL  byte(0)
+  #define ARRIVED_SYMBOL  byte(1)
+  #define EXIT_SYMBOL     byte(2)
+  #define CLOCK_SYMBOL    byte(3)
+  #define DINNER_SYMBOL   byte(4)
+  #define TEMP_C_SYMBOL   byte(5)
+
+Serial.println("lcd inited");
 }
 
 void loop() {
---
+++ work_timer2/src/types.h
@@ -0,0 +1,122 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+// LCD
+enum LedState {
+  ledStateReady,
+  ledStateWork,
+  ledStateDinner,
+};
+
+
+
+
+const TProgmemRGBPalette16 WorkColors_p FL_PROGMEM = {
+    CRGB::Orange,
+    CRGB::Maroon,
+
+    CRGB::DarkRed,
+    CRGB::Maroon,
+    CRGB::DarkRed,
+
+    CRGB::DarkRed,
+    CRGB::DarkRed,
+    CRGB::Red,
+    CRGB::Orange,
+
+    CRGB::White,
+    CRGB::Orange,
+    CRGB::Red,
+    CRGB::DarkRed
+};
+
+const TProgmemRGBPalette16 ReadyColors_p FL_PROGMEM = {
+    CRGB::DarkGreen,
+    CRGB::DarkGreen,
+    CRGB::DarkOliveGreen,
+    CRGB::DarkGreen,
+
+    CRGB::Green,
+    CRGB::ForestGreen,
+    CRGB::OliveDrab,
+    CRGB::Green,
+
+    CRGB::SeaGreen,
+    CRGB::MediumAquamarine,
+    CRGB::LimeGreen,
+    CRGB::YellowGreen,
+
+    CRGB::LightGreen,
+    CRGB::LawnGreen,
+    CRGB::MediumAquamarine,
+    CRGB::ForestGreen
+};
+
+
+byte divider[8] = {
+  B01100,
+  B01100,
+  B01100,
+  B01100,
+  B01100,
+  B01100,
+  B01100,
+  B01100,
+};
+
+byte arrived[8] = {
+  B00000,
+  B00100,
+  B00010,
+  B11111,
+  B00010,
+  B00100,
+  B00000,
+  B00000,
+};
+
+byte exit_symbol[8] = {
+  B00000,
+  B00100,
+  B01110,
+  B10101,
+  B00100,
+  B00100,
+  B00100,
+  B00000,
+};
+
+byte clock_symbol[8] = {
+  B01110,
+  B10101,
+  B10101,
+  B10111,
+  B10001,
+  B10001,
+  B01110,
+  B00000,
+};
+
+byte dinner_symbol[8] = {
+  B10100,
+  B01010,
+  B00101,
+  B00000,
+  B00111,
+  B01111,
+  B10111,
+  B01111,
+};
+
+byte temp_symbol[8] = {
+  B00010,
+  B00101,
+  B00010,
+  B01000,
+  B11100,
+  B01000,
+  B01000,
+  B00110,
+};
+
+#endif
