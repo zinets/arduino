d7c6c8028faf78198fa1b2cc38be847564d72a72
zinets <zinetz.victor@gmail.com>
2019-01-12 13:34:23 +0200

    ++ wifi config

--- weather_station/sensor_balkony/src/main.cpp
+++ weather_station/sensor_balkony/src/main.cpp
@@ -5,6 +5,12 @@
 
 Adafruit_BMP280 bme;
 
+#include <WiFiManager.h>
+char  blynkToken[33] = "48ed6d953c2e4a5f8ff06fe5f2a8b415";
+
+
+void configModeCallback (WiFiManager *myWiFiManager);
+
 void setup() {
   Serial.begin(9600);
   Serial.println(F("BMP280 test"));
@@ -12,6 +18,21 @@ void setup() {
   if (!bme.begin()) {  
     Serial.println("Could not find a valid BMP280 sensor, check wiring!");
     while (1);
+  }
+
+  WiFiManager wifiManager;
+  wifiManager.setBreakAfterConfig(true);
+  wifiManager.setAPCallback(configModeCallback);
+
+  WiFiManagerParameter custom_blynk_text("<br/>Balcony  config. <br/>");
+  wifiManager.addParameter(&custom_blynk_text);
+  WiFiManagerParameter custom_blynk_token("blynk-token", "blynk token", blynkToken, 33);
+  wifiManager.addParameter(&custom_blynk_token);
+
+  if (!wifiManager.autoConnect()) {
+    delay(3000);
+    ESP.reset();
+    delay(5000);
   }
 }
 
@@ -30,4 +51,9 @@ void loop() {
     
     Serial.println();
     delay(2000);
+}
+
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Needs wifi setup");
 }
