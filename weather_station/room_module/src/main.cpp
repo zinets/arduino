35083f4c0d78c27bf6bdf94b400c941291d2cef6
zinets <zinetz.victor@gmail.com>
2019-02-03 15:16:21 +0200

    ++ tft connection

--- weather_station/room_module/src/main.cpp
+++ weather_station/room_module/src/main.cpp
@@ -15,27 +15,6 @@ float outdoor_temperature = 0;
 float outdoor_pressure = 0;
 float outdoor_humidity = 0;
 
-void setup() {
-  Serial.begin(9600); 
-
-  // wifi connection
-  WiFiManager wifiManager;
-
-  //reset settings - for testing
-  // wifiManager.resetSettings();
-
-  WiFiManagerParameter custom_blynk_token("blynk-token", "blynk token", blynkToken, 33);
-  wifiManager.addParameter(&custom_blynk_token);
-
-    if (!wifiManager.autoConnect("IndoorSensor#1", "134679852")) {
-      delay(3000);
-      ESP.reset();
-      delay(5000);
-  }
-
-  Blynk.config(blynkToken);
-}
-
 BLYNK_CONNECTED() {
   Blynk.syncVirtual(VIRTUAL_SENSOR_OUTDOOR_HUMIDITY, 
     VIRTUAL_SENSOR_OUTDOOR_HUMIDITY, 
@@ -58,6 +37,48 @@ BLYNK_WRITE(VIRTUAL_SENSOR_OUTDOOR_HUMIDITY) {
   outdoor_humidity = param.asFloat();
   Serial.print("V4 ");
   Serial.println(outdoor_humidity);
+}
+
+#include "Adafruit_GFX.h"
+#include "Adafruit_ST7735.h"
+#define TFT_CS     D8
+#define TFT_RST    D4
+#define TFT_DC     D3
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.setTextSize(2);
+  tft.print(text);
+}
+
+void setup() {
+  Serial.begin(9600); 
+
+  tft.initR(INITR_BLACKTAB);
+  tft.fillScreen(ST77XX_BLACK);
+
+  testdrawtext("Lorem ipsum dolor sit suada nunc bibendum. Nullam aliquet ultrices m ", ST77XX_WHITE);
+  
+  return;
+  // wifi connection
+  WiFiManager wifiManager;
+
+  //reset settings - for testing
+  // wifiManager.resetSettings();
+
+  WiFiManagerParameter custom_blynk_token("blynk-token", "blynk token", blynkToken, 33);
+  wifiManager.addParameter(&custom_blynk_token);
+
+    if (!wifiManager.autoConnect("IndoorSensor#1", "134679852")) {
+      delay(3000);
+      ESP.reset();
+      delay(5000);
+  }
+
+  Blynk.config(blynkToken);
 }
 
 void loop() {
