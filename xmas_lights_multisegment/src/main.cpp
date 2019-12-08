4e6b656e1a6fac5012a46077dc85929659cf0740
zinets <zinetz.victor@gmail.com>
2019-12-08 18:36:34 +0200

    ++ test for 5 branches

--- xmas_lights_multisegment/src/main.cpp
+++ xmas_lights_multisegment/src/main.cpp
@@ -1,9 +1,30 @@
-#include <Arduino.h>
+// MultipleStripsInOneArray - see https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples for more info on
+// using multiple controllers.  In this example, we're going to set up four NEOPIXEL strips on three
+// different pins, each strip will be referring to a different part of the single led array
+
+#include <FastLED.h>
+
+#define NUM_STRIPS 5
+#define START_PIN 2
+#define NUM_LEDS_PER_STRIP 17
+#define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
+
+CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
 void setup() {
-  // put your setup code here, to run once:
+  FastLED.addLeds<NEOPIXEL, 2>(leds, 0 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 3>(leds, 1 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 4>(leds, 2 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 5>(leds, 3 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 6>(leds, 4 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Blue;
+    FastLED.show();
+    // leds[i] = CRGB::Black;
+    delay(100);
+  }
 }
