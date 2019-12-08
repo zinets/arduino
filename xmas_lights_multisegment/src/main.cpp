0b1ec938a6b0bc7e14d94daba94a85da932ad487
zinets <zinetz.victor@gmail.com>
2019-12-08 19:01:22 +0200

    ++ нормально работающий восстановленный вариант

--- xmas_lights_multisegment/src/main.cpp
+++ xmas_lights_multisegment/src/main.cpp
@@ -8,8 +8,16 @@
 #define START_PIN 2
 #define NUM_LEDS_PER_STRIP 17
 #define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
+#define FRAMES_PER_SECOND  120
+// скорость изменения цвета
+#define COLOR_CHANGE_SPEED 50 
+// частота вспышек
+#define GLITTER_FREQUENCY 200
+// яркость вспышек
+#define GLITTER_INTENSITY 65
 
 CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
+uint8_t gHue = 0; 
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, 2>(leds, 0 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
@@ -18,13 +26,34 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, 5>(leds, 3 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
   FastLED.addLeds<NEOPIXEL, 6>(leds, 4 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
 
+  FastLED.setBrightness(100); // 0 - 255
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void rainbow() {
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter() {
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  EVERY_N_MILLISECONDS( GLITTER_FREQUENCY ) { addGlitter(GLITTER_INTENSITY); } 
+  
 }
 
 void loop() {
-  for(int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Blue;
-    FastLED.show();
-    // leds[i] = CRGB::Black;
-    delay(100);
-  }
+  rainbowWithGlitter();
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000 / FRAMES_PER_SECOND); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( COLOR_CHANGE_SPEED ) { gHue++; } // slowly cycle the "base color" through the rainbow
 }
