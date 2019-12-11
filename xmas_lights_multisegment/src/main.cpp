7133b88f09a1c78451818e1d3d489779b51562f5
zinets <zinetz.victor@gmail.com>
2019-12-11 19:34:44 +0200

    ++ 2 stipes test

--- xmas_lights_multisegment/src/main.cpp
+++ xmas_lights_multisegment/src/main.cpp
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 
-#define NUM_STRIPS 6
+#define NUM_STRIPS 1
 #define START_PIN 2
 #define NUM_LEDS_PER_STRIP 17
 #define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
@@ -15,19 +15,25 @@
 CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 uint8_t gHue = 0; 
 
+
+// gold stipes
+#define NUM_GOLD_LEDS 17
+CRGB goldLeds[NUM_GOLD_LEDS];
+
 void setup() {
   FastLED.addLeds<NEOPIXEL, 2>(leds, 0 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<NEOPIXEL, 3>(leds, 1 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<NEOPIXEL, 4>(leds, 2 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<NEOPIXEL, 5>(leds, 3 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<NEOPIXEL, 6>(leds, 4 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-  FastLED.addLeds<NEOPIXEL, 7>(leds, 5 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  // FastLED.addLeds<NEOPIXEL, 3>(leds, 1 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  // FastLED.addLeds<NEOPIXEL, 4>(leds, 2 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  // FastLED.addLeds<NEOPIXEL, 5>(leds, 3 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  // FastLED.addLeds<NEOPIXEL, 6>(leds, 4 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  // FastLED.addLeds<NEOPIXEL, 7>(leds, 5 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+
+  FastLED.addLeds<NEOPIXEL, 7>(goldLeds, 0, NUM_GOLD_LEDS);
 
   FastLED.setBrightness(100); // 0 - 255
 }
 
-void addGlitter( fract8 chanceOfGlitter) 
-{
+void addGlitter( fract8 chanceOfGlitter) {
   if( random8() < chanceOfGlitter) {
     leds[ random16(NUM_LEDS) ] += CRGB::White;
   }
@@ -45,8 +51,20 @@ void rainbowWithGlitter() {
   
 }
 
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy(goldLeds, NUM_GOLD_LEDS, 20);
+  byte dothue = 0;
+  for (int i = 0; i < 8; i++) {
+    goldLeds[beatsin16(i+7, 0, NUM_GOLD_LEDS - 1)] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
 void loop() {
   rainbowWithGlitter();
+  juggle();
+
   FastLED.show();  
   // insert a delay to keep the framerate modest
   FastLED.delay(1000 / FRAMES_PER_SECOND); 
