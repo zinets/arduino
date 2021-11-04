f37c72545fccd4ec74761e38dda6d2eda2227a33
Victor Zinets <victor.zinets@gmail.com>
2021-11-04 17:21:54 +0200

    ++ tests...

--- garage_clock/src/main.cpp
+++ garage_clock/src/main.cpp
@@ -3,13 +3,23 @@
 #include "digits/digits.h"
 
 RTC_DS1307 rtc;
+// DateTime time;
 
 #define DELAY 1500
 
+volatile int b = 0;
+
+IRAM_ATTR void updateTime() {
+  b = 1;
+}
+
 void setup() {
+  delay(1000);
+
   setupLed();
 
-  Serial.begin(115200);
+  Serial.begin(19200);
+  Serial.println("setup begin");
 
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
@@ -21,7 +31,15 @@ void setup() {
     Serial.println("RTC is NOT running, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
+
+  // rtc.writeSqwPinMode(DS1307_SquareWave1HZ);
+  // pinMode(RTC_SQUARE_PIN, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(RTC_SQUARE_PIN), updateTime, RISING);
+
+  Serial.println("setup done");
 }
+
+
 
 void loop() {
   // for (int i = 0; i < 10; i++) {
@@ -29,10 +47,16 @@ void loop() {
   
   //   delay(DELAY);
   // }
+
   DateTime time = rtc.now();
   Serial.println(String("DateTime::TIMESTAMP_TIME:\t")+time.timestamp(DateTime::TIMESTAMP_FULL));
 
   displayTime(time);
 
   delay(1000);
+
+  // if (b) {
+  //   Serial.println("piu");
+  //   b = 0;
+  // }
 }
