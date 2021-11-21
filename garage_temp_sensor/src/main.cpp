52e933883745f267fcd427127468afae664686af
zinets <zinetz.victor@gmail.com>
2021-11-21 19:46:37 +0200

    ++ dht11 sensor (unsucc)

--- garage_temp_sensor/src/main.cpp
+++ garage_temp_sensor/src/main.cpp
@@ -2,17 +2,46 @@
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include "Adafruit_I2CDevice.h"
 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
 #define cs   D8
 #define dc   D3
 #define rst  D4
+#define DHTPIN D1
+
+#define DHTTYPE    DHT11
+uint32_t delayMS;
 
 Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
+DHT_Unified dht(DHTPIN, DHTTYPE);
 
 void displayTemperature(float main_temp);
 
 void loop() {
-  displayTemperature(37.3);
-  delay(1500);
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
 }
 
 #define BLACK    0x0000
@@ -24,6 +53,8 @@ void loop() {
 #define YELLOW   0xFFE0
 #define WHITE    0xFFFF
 #define GREY     0xC618
+
+
 
 void setup(void) {
    Serial.begin(9600);
@@ -40,6 +71,33 @@ void setup(void) {
   tft.setTextColor(WHITE);
   tft.setTextSize(1);
   tft.print("Ready");
+
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
 }
 
 void displayTemperature(float main_temp) {
